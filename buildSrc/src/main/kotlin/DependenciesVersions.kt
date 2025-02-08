122cd880c199d17adb55ef1a1b3836db78e11be3
adicomdotir <adicom.ir@gmail.com>
2025-02-08 20:54:58 +0330

    Init Commit

---
+++ buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -0,0 +1,11 @@
+object DependenciesVersions {
+    const val CORE_KTX = "1.15.0"
+    const val LIFE_CYCLE_RUNTIME_KTX = "2.8.7"
+    const val KOTLIN = "2.0.0"
+    const val JUNIT = "4.13.2"
+    const val JUNIT_VERSION = "1.2.1"
+    const val ESPRESSO_CORE = "3.6.1"
+    const val ACTIVITY_COMPOSE = "1.10.0"
+    const val COMPOSE_UI = "1.6.8"
+    const val MATERIAL_3 = "1.2.1"
+}
