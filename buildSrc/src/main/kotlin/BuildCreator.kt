44a728f42315e36e9732cca54fe805b6c98bb4de
adicomdotir <adicom.ir@gmail.com>
2025-03-26 22:58:44 +0330

    chore: add build parameter

--- buildSrc/src/main/kotlin/BuildCreator.kt
+++ buildSrc/src/main/kotlin/BuildCreator.kt
@@ -16,6 +16,23 @@ sealed class BuildCreator(val name:String) {
                 versionNameSuffix = Build.Debug.versionNameSuffix
                 applicationIdSuffix = Build.Debug.applicationIdSuffix
                 enableUnitTestCoverage = Build.Debug.enableUnitTestCoverage
+
+                buildConfigStringField(
+                    BuildVariables.BASE_URL,
+                    project.getLocalProperty("dev.debug_endpoint")
+                )
+                buildConfigIntField(
+                    BuildVariables.DB_VERSION,
+                    project.getLocalProperty("dev.db_version")
+                )
+                buildConfigBooleanField(
+                    BuildVariables.CAN_CLEAR_CACHE,
+                    project.getLocalProperty("dev.clear_cache")
+                )
+                buildConfigStringField(
+                    BuildVariables.MAP_KEY,
+                    project.getLocalProperty("dev.map_key")
+                )
             }
         }
     }
@@ -26,6 +43,23 @@ sealed class BuildCreator(val name:String) {
                 isMinifyEnabled = Build.Release.isMinifyEnabled
                 enableUnitTestCoverage = Build.Release.enableUnitTestCoverage
                 isDebuggable = Build.Release.isDebuggable
+
+                buildConfigStringField(
+                    BuildVariables.BASE_URL,
+                    project.getLocalProperty("dev.prod_endpoint")
+                )
+                buildConfigIntField(
+                    BuildVariables.DB_VERSION,
+                    project.getLocalProperty("dev.db_version")
+                )
+                buildConfigBooleanField(
+                    BuildVariables.CAN_CLEAR_CACHE,
+                    project.getLocalProperty("dev.clear_cache")
+                )
+                buildConfigStringField(
+                    BuildVariables.MAP_KEY,
+                    project.getLocalProperty("release.map_key")
+                )
             }
         }
     }
@@ -38,6 +72,23 @@ sealed class BuildCreator(val name:String) {
                 isDebuggable = Build.ReleaseExternalQa.isDebuggable
                 versionNameSuffix = Build.ReleaseExternalQa.versionNameSuffix
                 applicationIdSuffix = Build.ReleaseExternalQa.applicationIdSuffix
+
+                buildConfigStringField(
+                    BuildVariables.BASE_URL,
+                    project.getLocalProperty("dev.qa_endpoint")
+                )
+                buildConfigIntField(
+                    BuildVariables.DB_VERSION,
+                    project.getLocalProperty("dev.db_version")
+                )
+                buildConfigBooleanField(
+                    BuildVariables.CAN_CLEAR_CACHE,
+                    project.getLocalProperty("dev.clear_cache")
+                )
+                buildConfigStringField(
+                    BuildVariables.MAP_KEY,
+                    project.getLocalProperty("dev.map_key")
+                )
             }
         }
     }
